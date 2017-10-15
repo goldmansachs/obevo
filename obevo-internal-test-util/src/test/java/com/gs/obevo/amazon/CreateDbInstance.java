cac982a517283b53a5233ccb105668d42f211613
shantstepanian <17996546+shantstepanian@users.noreply.github.com>
2017-10-15 11:04:51 -0400

    Amazon file cleanup

--- obevo-internal-test-util/src/test/java/com/gs/obevo/amazon/CreateDbInstance.java
+++ obevo-internal-test-util/src/test/java/com/gs/obevo/amazon/CreateDbInstance.java
@@ -25,18 +25,83 @@ import com.amazonaws.services.rds.model.DescribeDBInstancesResult;
 
 public class CreateDbInstance {
     private static final AmazonRDS client = AmazonRDSClientBuilder.defaultClient();
-    public static final String dbInstanceIdentifier = "dbdeploy-oracle-12-1";
+    public static final String oracleDbInstanceId = "dbdeploy-oracle-12-1";
+    public static final String postgresDbInstanceId = "dbdeploy-postgres-9-6";
+    public static final String sqlserverDbInstanceId = "dbdeploy-sqlserver-13-0";
 
     public static void main(String[] args) throws Exception {
-/*
-        create();
-        describe();
-*/
-
-        delete();
+        CreateDbInstance instance = new CreateDbInstance();
+//        instance.createOracle(oracleDbInstanceId);
+        instance.createSqlServer(sqlserverDbInstanceId);
+//        instance.createPostgresql(postgresDbInstanceId);
+//        instance.describe(oracleDbInstanceId);
+//        instance.delete(sqlserverDbInstanceId);
     }
 
-    public static void describe() throws Exception {
+    public void createOracle(String dbInstanceIdentifier) throws Exception {
+        // http://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
+        CreateDBInstanceRequest request = new CreateDBInstanceRequest()
+                .withEngine("oracle-se2")
+                //.withEngineVersion("12.1.0.2.v8")
+                .withLicenseModel("license-included")
+                .withAllocatedStorage(10)
+                .withStorageType("gp2")  // SSD
+                .withBackupRetentionPeriod(0)
+                .withDBInstanceClass("db.t2.micro")
+                .withDBInstanceIdentifier(dbInstanceIdentifier)
+                .withDBName("DBDEPLOY")
+                .withMasterUsername("deploybuilddbo")
+                .withMasterUserPassword("deploybuilddb0")
+                ;
+        DBInstance response = client.createDBInstance(request);
+        System.out.println(response);
+
+        describe(dbInstanceIdentifier);
+    }
+
+    public void createPostgresql(String dbInstanceIdentifier) throws Exception {
+        // http://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
+        CreateDBInstanceRequest request = new CreateDBInstanceRequest()
+                .withEngine("postgres")
+                .withEngineVersion("9.6.2")
+                .withLicenseModel("postgresql-license")
+                .withAllocatedStorage(5)
+                .withStorageType("gp2")  // SSD
+                .withBackupRetentionPeriod(0)
+                .withDBInstanceClass("db.t2.micro")
+                .withDBInstanceIdentifier(dbInstanceIdentifier)
+                .withDBName("DBDEPLOY")
+                .withMasterUsername("deploybuilddbo")
+                .withMasterUserPassword("deploybuilddb0")
+                ;
+        DBInstance response = client.createDBInstance(request);
+        System.out.println(response);
+
+        describe(dbInstanceIdentifier);
+    }
+
+    public void createSqlServer(String dbInstanceIdentifier) throws Exception {
+        // http://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
+        CreateDBInstanceRequest request = new CreateDBInstanceRequest()
+                .withEngine("sqlserver-ex")
+                .withEngineVersion("13.00.2164.0.v1")
+                .withLicenseModel("license-included")
+                .withAllocatedStorage(20)
+                .withStorageType("gp2")  // SSD
+                .withBackupRetentionPeriod(0)
+                .withDBInstanceClass("db.t2.micro")
+                .withDBInstanceIdentifier(dbInstanceIdentifier)
+                //.withDBName("DBDEPLOY")
+                .withMasterUsername("deploybuilddbo")
+                .withMasterUserPassword("deploybuilddb0")
+                ;
+        DBInstance response = client.createDBInstance(request);
+        System.out.println(response);
+
+        describe(dbInstanceIdentifier);
+    }
+
+    public void describe(String dbInstanceIdentifier) throws Exception {
         while (true) {
             DescribeDBInstancesRequest request = new DescribeDBInstancesRequest()
                     .withDBInstanceIdentifier(dbInstanceIdentifier);
@@ -55,28 +120,7 @@ public class CreateDbInstance {
         }
     }
 
-    public static void create() throws Exception {
-        // http://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
-        CreateDBInstanceRequest request = new CreateDBInstanceRequest()
-                .withEngine("oracle-se2")
-                //.withEngineVersion("12.1.0.2.v8")
-                .withLicenseModel("license-included")
-                .withAllocatedStorage(10)
-                .withStorageType("gp2")  // SSD
-                .withBackupRetentionPeriod(0)
-                .withDBInstanceClass("db.t2.micro")
-                .withDBInstanceIdentifier(dbInstanceIdentifier)
-                .withDBName("DBDEPLOY")
-                .withMasterUsername("deploybuilddbo")
-                .withMasterUserPassword("deploybuilddb0")
-                ;
-        DBInstance response = client.createDBInstance(request);
-        System.out.println(response);
-
-        describe();
-    }
-
-    public static void delete() {
+    public void delete(String dbInstanceIdentifier) {
         DBInstance dbInstance = client.deleteDBInstance(
                 new DeleteDBInstanceRequest(dbInstanceIdentifier).withSkipFinalSnapshot(true)
         );
