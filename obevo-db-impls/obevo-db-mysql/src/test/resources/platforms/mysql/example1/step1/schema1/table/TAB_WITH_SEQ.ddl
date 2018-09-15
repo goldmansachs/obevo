0fc574e9eb6ef579067ec313b37ee2d37f65d38c
shantstepanian <17996546+shantstepanian@users.noreply.github.com>
2018-09-15 11:20:26 -0400

    MySQL example test files

---
+++ obevo-db-impls/obevo-db-mysql/src/test/resources/platforms/mysql/example1/step1/schema1/table/TAB_WITH_SEQ.ddl
@@ -0,0 +1,8 @@
+//// METADATA excludePlatforms=REDSHIFT
+//// CHANGE name=chng1
+CREATE TABLE TAB_WITH_SEQ (
+	ID           	int4 NOT NULL DEFAULT nextval('MYSEQ1'::regclass),
+	FIELD1    INT	NULL,
+    PRIMARY KEY (ID)
+)
+GO
---
+++ obevo-db-impls/obevo-db-mysql/src/test/resources/platforms/mysql/example1/step1/schema1/view/VIEW_DEPENDING_ON_OTHER_SCHEMA.sql
@@ -0,0 +1,18 @@
+--
+-- Copyright 2017 Goldman Sachs.
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
+//// METADATA includeDependencies="schema2.VIEW1"
+CREATE OR REPLACE VIEW VIEW_DEPENDING_ON_OTHER_SCHEMA AS SELECT * FROM ${schema2_physicalName}.VIEW1
